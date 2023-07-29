de9fdbd790fee55262da26edda71a4dfa4df4b8f
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-07-29 14:29:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -511,15 +511,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         caption=script.CHANNEL_CAP.format(query.from_user.mention, title, query.message.chat.title),
                         protect_content=True if ident == "filep" else False,
                         reply_markup=InlineKeyboardMarkup(
-                        buttons = [[
-                            InlineKeyboardButton('mal', callback_data='minfo'), 
-                            InlineKeyboardButton('hin', callback_data='reqinfo'), 
-                            InlineKeyboardButton('tam', callback_data='gxneopm')
-                        ], [
-                            InlineKeyboardButton('join main channel', url='https://t.me/cenEma9')
-                        ]]
-                        ) 
-                    ) 
+                            [
+                                [
+                                    InlineKeyboardButton("ᴊᴏɪɴ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ", url='https://t.me/cenEma9')
+                                ]
+                            ]
+                        )
+                    )
                     Joel_tgx = await query.message.reply_text(
                         script.FILE_MSG.format(query.from_user.mention, title, size, currentTime),
                         parse_mode=enums.ParseMode.HTML,
