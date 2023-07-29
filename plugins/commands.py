65b3e14f7b191be93a2ae7b81e7930fa1b8bcfd4
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-07-29 11:55:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,10 +44,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('💢 Cʟɪᴄᴋ Tᴏ Vɪᴇᴡ Mᴏʀᴇ Bᴜᴛᴛᴏɴs 💢', callback_data='start')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA") 
-        await asyncio.sleep(1)
-        await m.delete()        
+        reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
